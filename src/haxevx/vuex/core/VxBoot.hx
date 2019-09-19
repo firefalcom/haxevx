e29738e01e8ec7f4b0e55c744f44fc01567e1a66
Julien Hamaide <julien.hamaide@gmail.com>
2019-09-19 10:39:05 +0200

    Support for router

--- src/haxevx/vuex/core/VxBoot.hx
+++ src/haxevx/vuex/core/VxBoot.hx
@@ -11,40 +11,40 @@ import haxevx.vuex.native.Vuex.Store;
 class VxBoot
 {
 	public function new() {
-		
+
 	}
 	// saved parameter cache
 	 var STORE:Store<Dynamic>;
 	//public static var VUE:VComponent<Dynamic,Dynamic>;
-	
+
 	 public static inline function registerModuleWithStore(name:String, mod:Dynamic, store:Store<Dynamic>):Void {
 		mod._Init(name+"/");
 		untyped store[name] = mod; // Reflect.setField(store, name, mod);
 		store.registerModule(name, mod);
 		clearModuleInjStack(store);
-		
+
 	 }
-	
-	
+
+
 	public function startStore<T>(storeParams:Dynamic):Store<Dynamic>  {
 		if (STORE != null) {
 			throw "Vuex store already started! Only 1 store is allowed";
 		}
-		
+
 		var metaFields:Dynamic<Array<Dynamic>>;
 		var md:Dynamic;
 		var noNamespaceGetterProps:Dynamic;
 		// use Vuex
 		//Vue.use(Vuex);
-	
+
 
 		var store = new Store(storeParams);
-		
+
 		// below will depeeciate
-				
+
 		//if ( storeParams.getters!= null) {
 			// define get_ utility functions under store.getters ( for non-namespced
-			
+
 			var o:Dynamic;
 			// define get_utility functions under store.customGetters to store.getters namespaced
 			//Reflect.field(opts.storeParams.getters
@@ -54,56 +54,57 @@ class VxBoot
 			var i:Int = stack.length;
 			while (--i > -1) {
 				stack[i]._InjNative(storeGetters);
-			
+
 			}
 			stack = [];
-				
-			// below should be depcirated, ie. avoid recursion into tree 
-			
+
+			// below should be depcirated, ie. avoid recursion into tree
+
 			if (storeParams.modules != null) {  // todo: modules and fractal depth first traversal
 				//var moduleNameStack:Array<String> = [];
-				
+
 				for (p in Reflect.fields((o = storeParams.modules))) {
 					//moduleNameStack.push(p);
-					
+
 					var m:NativeModule<Dynamic,Dynamic> = Reflect.field(o, p);
 					md = untyped storeParams[p];// Reflect.field(storeParams, p); //VModule<Dynamic>
 					untyped store[p] = md;
 
 					//if (m.getters != null) {
-										
+
 					//	noNamespaceGetterProps = GetterFactory.setupGettersFromInstance(md);
-						
+
 						//untyped md._stg = storeGetters;
 					//	md._InjNative(storeGetters);
 
 					//}
-			
+
 					//moduleNameStack.pop();
-					
+
 				}
 			}
-			
+
 		//}
 
 		STORE = store;
 		return store;
 	}
-	
-	
+
+
 	 static function clearModuleInjStack(store:Store<Dynamic>):Void {
 		 var storeGetters = store.getters;
 		var stack = ModuleStack.stack;
 		var i:Int = stack.length;
 		while (--i > -1) {
 			stack[i]._InjNative(storeGetters);
-		
+
 		}
 		ModuleStack.stack = [];
 	}
-	
-	public function startVueWithRootComponent<T>(el:String, rootComponent:VComponent<Dynamic, Dynamic>):Vue {
-		var bootVueParams:Dynamic = {};
+
+	public function startVueWithRootComponent<T>(el:String, rootComponent:VComponent<Dynamic, Dynamic>, ?params: Dynamic):Vue {
+		var bootVueParams : Dynamic = params == null ? {} : params;
+
 		bootVueParams.el = el;
 		if (STORE != null) {
 			Reflect.setField(bootVueParams, "store", STORE);
@@ -114,13 +115,13 @@ class VxBoot
 		notifyStarted();
 		return vm;
 	}
-	
+
 	public static function notifyStarted():Void {
 		#if  !(production || skip_singleton_check )
 			haxevx.vuex.core.Singletons.clearLookups( );
 		#end
 	}
-	
+
 	public  static function getRenderComponentMethod(nativeComp:Dynamic):CreateElement->VNode {
 		return function(h:CreateElement):VNode {
 			return h(nativeComp,null,null);
